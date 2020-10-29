85458efcc2048ee9bb369533e243571702c8f50e
Huang Feilong <huangfeilong@gmail.com>
2020-10-30 07:32:05 +0800

    fix admin template sidebar toggle bug

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -9,8 +9,8 @@ require("@rails/activestorage").start()
 require("channels")
 require("bootstrap/dist/js/bootstrap.bundle")
 require("jquery.easing/jquery.easing")
-require('@fortawesome/fontawesome-free/js/all')
-require('startbootstrap-sb-admin-2/js/sb-admin-2')
+//require('@fortawesome/fontawesome-free/js/all')
+require("./sba2/sb-admin-2")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
